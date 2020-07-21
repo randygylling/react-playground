4668401931cd23c8e4df1ed81ec87a340a99eadb
Randy Gylling <randymgylling@gmail.com>
2020-07-21 09:34:17 -0700

    adding apollo with ricknmorty api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+query {
+  characters(page: 2, filter: { name: "rick" }) {
+    info {
+      count
+    }
+    results {
+      name
+    }
+  }
+  character(id: 1) {
+    id
+  }
+}
+`;
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data, 'ran twice')
+    return data.characters.results.map(({ name }, i) => (
+      <div key={name + i}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    ));
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql/',
+  cache: new InMemoryCache()
+});
 ReactDOM.render(
   <React.StrictMode>
+        <ApolloProvider client={client}>
+
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
